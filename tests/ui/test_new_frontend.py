0380506c1e54f2aa7b5aa8174350e1cd2404a715
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-08-01 13:41:09 -0700

    Added artifact collection

--- tests/ui/test_new_frontend.py
+++ tests/ui/test_new_frontend.py
@@ -4,4 +4,4 @@ from pages.desktop.home import Home
 def test_login(my_base_url, selenium):
     """User can login"""
     selenium.get(my_base_url)
-    assert 'Add-ons for Firefox' in selenium.title
+    assert False
