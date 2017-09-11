60be8634d2532f39ba8f22d417b1d423d48a04e5
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-09-11 14:37:37 -0700

    Changed geckodriver url

--- tests/ui/circleci/install-test-depenencies.sh
+++ tests/ui/circleci/install-test-depenencies.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -ex
 
-GECKODRIVER_URL=$(
-  curl -s 'https://api.github.com/repos/mozilla/geckodriver/releases/latest' |
-  python -c "import sys, json; r = json.load(sys.stdin); print([a for a in r['assets'] if 'linux64' in a['name']][0]['browser_download_url']);"
-);
-
-wget -O geckodriver.tar.gz $GECKODRIVER_URL
+wget -O geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux64.tar.gz
 gunzip -c geckodriver.tar.gz | tar xopf -
 chmod +x geckodriver
 sudo mv geckodriver /bin
