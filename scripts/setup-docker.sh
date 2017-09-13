c5a4c9f7885c9b8e9bc578d9010a538f10c98d9b
Christopher Grebs <cg@webshox.org>
2017-09-14 00:18:32 +0200

    Use setup-docker.sh

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -3,12 +3,12 @@
 # initialize_db:
 python manage.py reset_db --noinput
 python manage.py syncdb --noinput
+schematic --fake src/olympia/migrations/
+
 python manage.py loaddata initial.json
 python manage.py import_prod_versions
-schematic --fake src/olympia/migrations/
+python manage.py loaddata zadmin/users
+python manage.py update_permissions_from_mc
 
 # update_assets:
 make update_assets
-
-#populate_data:
-make populate_data
