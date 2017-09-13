2c682e401ede3e6527c38536dea8c4716a28aa8c
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-09-13 16:12:44 -0700

    Move nvm install

--- scripts/ui-test.sh
+++ scripts/ui-test.sh
@@ -2,9 +2,4 @@
 echo 127.0.0.1 olympia.dev | tee -a /etc/hosts
 export DISPLAY=:99
 docker-compose exec web pip install tox
-docker-compose exec web yum -y install curl
-curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh > install-nvm.sh
-docker-compose exec web bash /code/install-nvm.sh
-docker-compose exec web nvm install node_modules
-docker-compose exec web EXEC $SHELL
 docker-compose exec web tox -e ui-tests-1
