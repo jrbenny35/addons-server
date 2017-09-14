c6a3b485d7ce96eed037ddf5e7a62c7301848d21
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-09-14 09:44:45 -0700

    Updated scripts

--- scripts/ui-test.sh
+++ scripts/ui-test.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 echo 127.0.0.1 olympia.dev | tee -a /etc/hosts
-export DISPLAY=:99
-docker-compose exec web pip install tox
-docker-compose exec web tox -e ui-tests-1
+export DISPLAY=:
+yum -y install curl
+curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh > install-nvm.sh
+sh install-nvm.sh
+source ~/.bash_profile
+nvm install node
+unset NPM_CONFIG_PREFIX
+pip install tox
+sh scripts/setup-docker.sh
+tox -e ui-tests-1
